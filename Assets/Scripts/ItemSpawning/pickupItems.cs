68c44b0269a79f0d34e6e3be425aced55ad77bf8
Jacob Will <s171590@ad.aie.edu>
2017-04-19 17:02:24 +0930

    got box working

---
+++ Assets/Scripts/ItemSpawning/pickupItems.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupItems : MonoBehaviour {
+
+    public int score;
+    public float rotSpeed;
+    public bool DoYouWantBob;
+    public float bobCentre;
+    public float bobSpeed;
+    
+    float bobHeight;
+    bool GoUp;
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+    {
+
+        PlayerStats stats = other.GetComponent<PlayerStats>();
+
+        if(stats)
+        {
+            stats.AddScore(score);
+        }
+
+       gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        transform.Rotate(0, (rotSpeed * Time.deltaTime), 0);
+        if (DoYouWantBob)
+        {
+            Vector3 pos = transform.position;
+            pos.y = Mathf.Sin(Time.time * bobSpeed) + bobCentre;
+            transform.position = pos;
+        }
+
+  
+    }
+}
